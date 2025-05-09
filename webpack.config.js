f6e5bcd4fa4c045e93342514e958037bfa30bfdb
Ylva Selling <ylva.selling@gmail.com>
2025-05-09 11:17:31 +0200

    Dummy change to trigger build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const webpack = require('webpack');
-const path = require('path');
-const fs = require('fs');
+const webpack = require("webpack");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
-  entry: './backend.js',
-  target: 'node',
+  entry: "./backend.js",
+  target: "node",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'backend.js'
+    path: path.join(__dirname, "dist"),
+    filename: "backend.js",
   },
-  mode: "production"
-}
+  mode: "production",
+};
